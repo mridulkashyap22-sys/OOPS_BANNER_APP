3d2a57856c12c53b6b12524ec960cbe6772f5450
Mridul Kashyap <mridulkashyap2005@gmail.com>
2026-03-11 01:21:55 +0530

    implemented uc7

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,63 +1,64 @@
-public class OopsBanner {
+#include <iostream>
+#include <vector>
+using namespace std;
 
-    public static void main(String[] args) {
+class CharacterPattern {
+public:
+    char character;
+    string pattern[5];
 
-        String[] O = getO();
-        String[] P = getP();
-        String[] S = getS();
-
-        String[] banner = new String[O.length];
-
-        for (int i = 0; i < O.length; i++) {
-            banner[i] = String.join("   ",
-                    O[i],   // First O
-                    O[i],   // Second O
-                    P[i],
-                    S[i]
-            );
-        }
-
-        for (String line : banner) {
-            System.out.println(line);
+    CharacterPattern(char c, string p[5]) {
+        character = c;
+        for(int i = 0; i < 5; i++) {
+            pattern[i] = p[i];
         }
     }
+};
 
-    // Method for O
-    public static String[] getO() {
-        return new String[]{
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
+int main() {
+
+    string O[5] = {
+        " *** ",
+        "*   *",
+        "*   *",
+        "*   *",
+        " *** "
+    };
+
+    string P[5] = {
+        "**** ",
+        "*   *",
+        "**** ",
+        "*    ",
+        "*    "
+    };
+
+    string S[5] = {
+        " ****",
+        "*    ",
+        " *** ",
+        "    *",
+        "**** "
+    };
+
+    vector<CharacterPattern> patterns;
+
+    patterns.push_back(CharacterPattern('O', O));
+    patterns.push_back(CharacterPattern('P', P));
+    patterns.push_back(CharacterPattern('S', S));
+
+    string word = "OOPS";
+
+    for(int i = 0; i < 5; i++) {
+        for(char c : word) {
+            for(auto &cp : patterns) {
+                if(cp.character == c) {
+                    cout << cp.pattern[i] << "  ";
+                }
+            }
+        }
+        cout << endl;
     }
 
-    // Method for P
-    public static String[] getP() {
-        return new String[]{
-                "****** ",
-                "*     *",
-                "*     *",
-                "****** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-    }
-
-    // Method for S
-    public static String[] getS() {
-        return new String[]{
-                " ***** ",
-                "*     *",
-                "*      ",
-                " ***** ",
-                "      *",
-                "*     *",
-                " ***** "
-        };
-    }
+    return 0;
 }
